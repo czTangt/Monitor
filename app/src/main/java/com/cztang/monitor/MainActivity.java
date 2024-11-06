6a17da17a0b2385498100be9e047eb4ac1bd657f
czTangt <cz.tangt@gmail.com>
2024-11-06 23:19:41 +0800

    prepare the project

--- app/src/main/java/com/cztang/monitor/MainActivity.java
+++ app/src/main/java/com/cztang/monitor/MainActivity.java
@@ -9,10 +9,9 @@ import com.cztang.monitor.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'monitor' library on application startup.
-    static {
-        System.loadLibrary("monitor");
-    }
+//    static {
+//        System.loadLibrary("monitor");
+//    }
 
     private ActivityMainBinding binding;
 
@@ -23,14 +22,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'monitor' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
